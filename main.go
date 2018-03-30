ed4e79328e77b9110e15a4301b865750b3420325
Vincent Camus <vincent.camus@krono-safe.com>
2018-03-30 12:01:44 +0200

    more play with js, POST and such

--- main.go
+++ main.go
@@ -3,13 +3,50 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
+var persons []Person
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "GET":
+		http.ServeFile(w, r, "web/index.html")
+	case "POST":
+		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		// Create a new Person object
+		p := Person{
+			Name:        r.FormValue("PersonName"),
+			Description: r.FormValue("PersonDescription")}
+		persons = append(persons, p)
+		for _, p := range persons {
+			jsonData, _ := json.Marshal(p)
+			fmt.Println(string(jsonData))
+		}
+
+		http.ServeFile(w, r, "web/index.html")
+	default:
+		fmt.Fprintf(os.Stdout, "Sorry, only GET and POST methods are supported.")
+	}
+}
+
 func main() {
-	p := Person{
-		Name:        "Vincent Camus",
-		Description: "C'est moi",
-		Picture:     "https://ih0.redbubble.net/image.394665545.8115/flat,800x800,075,f.jpg"}
-	jsonData, _ := json.Marshal(p)
-	fmt.Println(string(jsonData))
+	/*
+		p := Person{
+			Name:        "Vincent Camus",
+			Description: "C'est moi",
+			Picture:     "https://ih0.redbubble.net/image.394665545.8115/flat,800x800,075,f.jpg"}
+		jsonData, _ := json.Marshal(p)
+		fmt.Println(string(jsonData))
+	*/
+
+	http.HandleFunc("/", rootHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
